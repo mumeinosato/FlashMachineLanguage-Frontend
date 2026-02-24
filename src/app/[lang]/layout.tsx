6185d8d061f00663b9c9d5d8008dbd5e0826ff9b
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2026-02-24 21:57:49 +0900

    SpeedInsightsコンポーネントをRootLayoutに追加し、page.tsxから削除

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -2,6 +2,7 @@ import { dir } from 'i18next';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./../globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -26,7 +27,10 @@ export default async function RootLayout({children, params: paramsPromise,}: Rea
             {/* eslint-disable-next-line @next/next/no-sync-scripts */}
             <script src="/wasm/wasm_exec.js"></script>
         </head>
-        <body>{children}</body>
+        <body>
+            <SpeedInsights />
+            {children}
+        </body>
         </html>
     );
 }
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,6 +1,5 @@
 import { LanguageProvider } from "@/app/i18n/client";
 import ClientComponent from "@/app/[lang]/_components";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default async function Home({ params }: { params: Promise<{ lang: string }> }) {
     const { lang } = await params;
@@ -9,7 +8,6 @@ export default async function Home({ params }: { params: Promise<{ lang: string 
             <LanguageProvider initialLanguage={lang}>
                 <ClientComponent />
             </LanguageProvider>
-            <SpeedInsights />
         </main>
     );
 }
